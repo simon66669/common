b12ffc100f677c264f50062e3284ae169e0b6abe
sss <123@gmail.com>
2022-05-14 03:19:07 +0800

    1

--- redis/connect.go
+++ redis/connect.go
@@ -2,13 +2,15 @@ package predis
 
 import "github.com/go-redis/redis"
 
+var Rdb *redis.Client
+
 func InitRedis() *redis.Client {
 
-	rdb := redis.NewClient(&redis.Options{
+	Rdb = redis.NewClient(&redis.Options{
 		Addr:     "127.0.0.1:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
 
-	return rdb
+	return Rdb
 }
