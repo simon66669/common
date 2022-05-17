3b038294501f50734f77611aa72a53f7c5e09d23
sss <123@gmail.com>
2022-05-18 04:07:36 +0800

    1

--- mysql/connect.go
+++ mysql/connect.go
@@ -7,7 +7,7 @@ import (
 
 func Connect() (*gorm.DB, error) {
 
-	dsn := "exchange:123456@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:123456@tcp(127.0.0.1:3306)/exchange?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	return db, err
